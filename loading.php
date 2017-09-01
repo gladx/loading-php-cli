54d96fe9576f13639c22afc1e8968fac94c7c19a
iglad <aliyousefi.tec@gmail.com>
2017-09-02 02:50:09 +0430

    complete version 1

--- loading.php
+++ loading.php
@@ -1,15 +1,59 @@
 <?php
 
-function loadingDot(){
-    for ($i=0; $i < 10000; $i++) {
-        $delay = rand(100000, 300000);
-        echo ".  ", "Loading\r";
-        usleep($delay);
-        echo ".. ", "Loading\r";
-        usleep($delay);
-        echo "...", "Loading\r";
-        usleep($delay);
-    }
+function style($style= 'dot')
+{
+        switch ($style) {
+            case 'dot':
+                loading();
+                break;
+            case 'dash':
+                loading('-', ' ', 3, 10);
+                break;
+            case 'pipe':
+                loading('|', '.', 40, 2, 1000);
+                break;
+            case 'bar1' :
+                loading($str = '▉', $space = ' ');
+                break;
+            case 'bar2' :
+                loading($str = '▊', $space = '░');
+                break;
+            case 'bar3' :
+                loading($str = '▓', $space = '░');
+                break;
+
+        }
 }
 
-loadingDot();
+function loading($str = '.', $space = ' ', $pad = 20, $repeat = 1, $delay = 100000)
+{
+    for ($r=0; $r < $repeat; $r++) { // Repeat Loop
+        for ($i=0; $i <= $pad; $i++) {
+            $pre = '';
+            for ($k=0; $k < $i; $k++) {
+                $pre .= $str ;
+            }
+            for ($j=$i; $j < $pad; $j++) {
+                $pre .= $space;
+            }
+            echo "\033[?25l", '[', $pre, ']', " Loading\r";  // "\033[?25l" Hide the cursor.
+            usleep($delay);
+        }
+    
+        echo "\033[K" ; // "\033[K"  Delete everything from the cursor to the end of the line.
+    }
+    echo "\033[?25h"; //  "\033[?25h" Show the cursor.
+}
+
+style('dot');
+style('dash');
+style('pipe');
+style('bar1');
+style('bar2');
+style('bar3');
+
+
+
+
+
+
